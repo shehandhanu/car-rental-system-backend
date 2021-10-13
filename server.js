36b1ec3d8317079bddf1b09c4165b650fa7de6d9
Shahan Dhanuddara <65756207+shehandhanu@users.noreply.github.com>
2021-10-13 22:44:21 +0530

    port fixed

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ dotenv.config({ path: 'config/config.env' });
 
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 4000, () => {
     console.log(`Server Started at PORT ${process.env.PORT}`);
 })
